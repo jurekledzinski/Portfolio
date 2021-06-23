e6f344284f61a76dea17e6047fab72be39105348
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-23 12:18:56 +0200

    Create file Content.scss and add styles for main and main cover with linear gradient

--- client/src/components/content/Content.jsx
+++ client/src/components/content/Content.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Content.scss";
+
 import HomeSection from "./homeSection/HomeSection";
 import AboutSection from "./aboutSection/AboutSection";
 import ProjectsSection from "./projectsSection/ProjectsSection";
@@ -7,8 +9,8 @@ import ContactSection from "./contactSection/ContactSection";
 
 const Content = () => {
   return (
-    <main className="main" style={{ position: "relative" }}>
-      <div className="cover"></div>
+    <main className="main">
+      <div className="main__cover"></div>
       <HomeSection />
       <AboutSection />
       <ProjectsSection />
