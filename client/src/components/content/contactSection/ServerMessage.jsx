b47db7155973026d409f6cdf7af81ac5e722f6f1
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-25 13:47:09 +0200

    Create component for server messages to display in email form

---
+++ client/src/components/content/contactSection/ServerMessage.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+
+import "./ServerMessage.scss";
+
+const ServerMessage = () => {
+  let dataMsgServer = useSelector((store) => store.serverMsgData);
+  const { errorServerMsg, successServerMsg } = dataMsgServer;
+
+  console.log(dataMsgServer, "dataMsgServer server message component");
+
+  return (
+    <Fragment>
+      {errorServerMsg ||
+        (successServerMsg && (
+          <p
+            className={
+              Boolean(errorServerMsg)
+                ? "message-server"
+                : "message-server message-server--success"
+            }
+          >
+            {errorServerMsg || successServerMsg}
+          </p>
+        ))}
+    </Fragment>
+  );
+};
+
+export default ServerMessage;
