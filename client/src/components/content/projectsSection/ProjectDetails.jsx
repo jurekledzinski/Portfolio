46e146dc3f8bb184acfe149a951521646c9946f1
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-25 13:48:14 +0200

    Create component ProjectDetails and styles

---
+++ client/src/components/content/projectsSection/ProjectDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./ProjectDetails.scss";
+
+const ProjectDetails = () => {
+  return <article className="project-details"></article>;
+};
+
+export default ProjectDetails;
