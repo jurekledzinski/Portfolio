951e79ac9f29c2885fd87c15dc6981099faae07a
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-25 13:53:01 +0200

    Create actions,reducer for server messages

--- client/src/reduxeStore/reducers/rootreducer.js
+++ client/src/reduxeStore/reducers/rootreducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import { isActiveHeaderWrapperReducer } from "./reducerIsActiveHeaderWrapper";
 import { sectionsReducer } from "./reducerSections";
+import { serverMessagesReducer } from "./serverMessagesReducer";
 
 export const rootReducer = combineReducers({
   headerWrapperData: isActiveHeaderWrapperReducer,
   sectionsData: sectionsReducer,
+  serverMsgData: serverMessagesReducer,
 });
