c52b26e6f774fc2dac70b59de4a79356006fc474
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-25 13:45:00 +0200

    Create array of skills data to render it dynamically

---
+++ client/src/components/content/aboutSection/AboutData.js
@@ -0,0 +1,31 @@
+export const skillsData = [
+  {
+    skill: "HTML",
+    stars: [1, 1, 1, 1, 1],
+  },
+  {
+    skill: "CSS",
+    stars: [1, 1, 1, 1, 0],
+  },
+  {
+    skill: "Javascript",
+    stars: [1, 1, 1, 1, 0],
+  },
+  {
+    skill: "React",
+    stars: [1, 1, 1, 1, 0],
+  },
+  {
+    skill: "Node.js, Express",
+    stars: [1, 1, 1, 0, 0],
+  },
+  { skill: "MongoDB, Mongoose", stars: [1, 1, 1, 0, 0] },
+  {
+    skill: "Webpack",
+    stars: [1, 1, 1, 0, 0],
+  },
+  {
+    skill: "Git",
+    stars: [1, 1, 1, 0, 0],
+  },
+];
