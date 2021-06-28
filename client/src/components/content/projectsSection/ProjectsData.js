f528209f9d24980a373047fb44ea6ce0a5a04ed8
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-28 15:36:58 +0200

    Create file to store projects data

---
+++ client/src/components/content/projectsSection/ProjectsData.js
@@ -0,0 +1,23 @@
+export const ImagesData = [
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2019/08/08/11/42/butterfly-4392802_960_720.jpg",
+    title: "Barber Shop",
+    id: 0,
+    number: 1,
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2021/02/07/15/47/reed-5991717_960_720.jpg",
+    title: "Course shop",
+    id: 1,
+    number: 2,
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/10/14/19/49/santorini-5655299_960_720.jpg",
+    title: "Weather app",
+    id: 2,
+    number: 3,
+  },
+];
