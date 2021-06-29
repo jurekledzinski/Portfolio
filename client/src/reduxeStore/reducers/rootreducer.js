7101f3f4dce15e13460608aa509224b8519c9897
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-29 08:15:28 +0200

    Create actions,reducers to store index clicked cart and isVisible cart

--- client/src/reduxeStore/reducers/rootreducer.js
+++ client/src/reduxeStore/reducers/rootreducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 
+import { getIndexCartReducer } from "./getIndexCartReducer";
 import { isActiveHeaderWrapperReducer } from "./reducerIsActiveHeaderWrapper";
 import { sectionsReducer } from "./reducerSections";
 import { serverMessagesReducer } from "./serverMessagesReducer";
+import { hideShowDetailsProjectReducer } from "./hideShowDetailsProjectReducer";
 
 export const rootReducer = combineReducers({
+  indexCartData: getIndexCartReducer,
   headerWrapperData: isActiveHeaderWrapperReducer,
   sectionsData: sectionsReducer,
   serverMsgData: serverMessagesReducer,
+  visibilityProjectDetailsData: hideShowDetailsProjectReducer,
 });
