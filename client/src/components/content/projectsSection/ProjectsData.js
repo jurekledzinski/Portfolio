a6a7bdfedd58aa1d73ec024a6b79791e0199f6b4
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-29 08:19:22 +0200

    Create proejctsData component to store informations about projects

--- client/src/components/content/projectsSection/ProjectsData.js
+++ client/src/components/content/projectsSection/ProjectsData.js
@@ -1,23 +1,53 @@
 export const ImagesData = [
   {
+    imgPreview:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
     imgUrl:
       "https://cdn.pixabay.com/photo/2019/08/08/11/42/butterfly-4392802_960_720.jpg",
     title: "Barber Shop",
-    id: 0,
-    number: 1,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui magni sed laboriosam in reprehenderit iure unde id nostrum voluptates officia optio quisquam modi amet, dolore quidem quae temporibus quo? Assumenda reiciendis quos ducimus ipsum! Eveniet quisquam distinctio aut sint fugit mollitia explicabo, sequi repellendus, aliquid laudantium, veritatis non voluptatibus quidem!",
+    usedTech: [
+      "React",
+      "React router",
+      "Redux",
+      "Firebase",
+      "Node,Express,Mongoose,Passport.js",
+      "Chart.js,React Datepicker",
+      "Scss",
+    ],
   },
   {
+    imgPreview:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
     imgUrl:
       "https://cdn.pixabay.com/photo/2021/02/07/15/47/reed-5991717_960_720.jpg",
     title: "Course shop",
-    id: 1,
-    number: 2,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo unde dolores aliquam iure assumenda ad totam harum, eveniet voluptatibus ipsam culpa corporis reprehenderit enim laudantium accusamus vitae tempora consectetur, odit obcaecati dolor rerum similique! Placeat perspiciatis id maxime consectetur officiis quisquam, exercitationem, recusandae soluta quam, fugit ab temporibus excepturi omnis?",
+    usedTech: [
+      "React",
+      "React router",
+      "React context",
+      "Firebase",
+      "Node,Express,Mongoose,Passport.js",
+      "Scss",
+    ],
   },
   {
+    imgPreview:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
     imgUrl:
       "https://cdn.pixabay.com/photo/2020/10/14/19/49/santorini-5655299_960_720.jpg",
     title: "Weather app",
-    id: 2,
-    number: 3,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident consequatur nam saepe tenetur. Enim ut nesciunt similique architecto amet sed placeat cupiditate cumque rem quos delectus ipsam voluptate, illo hic distinctio expedita quasi molestiae obcaecati maxime voluptates mollitia! Fuga, numquam. Deleniti earum vel autem excepturi, iste alias accusamus sunt!",
+    usedTech: [
+      "HTML",
+      "Scss",
+      "Javascript",
+      "Moment timezone",
+      "Node, Express",
+    ],
   },
 ];
