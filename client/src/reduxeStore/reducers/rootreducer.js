2e4bdba3dc8fffe3d7a1d4a5a01f17e003ad887d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-07-02 11:15:50 +0200

    Add contactWaveRefsReducer and featuresRefsReducer to rootReducer

--- client/src/reduxeStore/reducers/rootreducer.js
+++ client/src/reduxeStore/reducers/rootreducer.js
@@ -1,12 +1,16 @@
 import { combineReducers } from "redux";
 
 import { getIndexCartReducer } from "./getIndexCartReducer";
+import { featureRefsReducer } from "./featureRefsReducer";
+import { contactWaveRefsReducer } from "./contactWaveRefsReducer";
 import { isActiveHeaderWrapperReducer } from "./reducerIsActiveHeaderWrapper";
 import { sectionsReducer } from "./reducerSections";
 import { serverMessagesReducer } from "./serverMessagesReducer";
 import { hideShowDetailsProjectReducer } from "./hideShowDetailsProjectReducer";
 
 export const rootReducer = combineReducers({
+  contactRefsWaveData: contactWaveRefsReducer,
+  featureRefsData: featureRefsReducer,
   indexCartData: getIndexCartReducer,
   headerWrapperData: isActiveHeaderWrapperReducer,
   sectionsData: sectionsReducer,
