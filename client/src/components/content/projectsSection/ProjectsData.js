08e2b0c22b3ad0fe77e747345b377bfa129ea09e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-07-20 12:52:35 +0200

    Add images of projects to display in slider

--- client/src/components/content/projectsSection/ProjectsData.js
+++ client/src/components/content/projectsSection/ProjectsData.js
@@ -1,9 +1,9 @@
 export const ImagesData = [
   {
     imgPreview:
-      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Hairdresser-shop-project.png?alt=media&token=72699f15-f01b-4b98-9a1e-e085297b6308",
     imgUrl:
-      "https://cdn.pixabay.com/photo/2019/08/08/11/42/butterfly-4392802_960_720.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Hairdresser-shop-tablet.jpg?alt=media&token=d5d1b70e-1a28-44cc-83dd-59c513c7d5f8",
     title: "Barber Shop",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui magni sed laboriosam in reprehenderit iure unde id nostrum voluptates officia optio quisquam modi amet, dolore quidem quae temporibus quo? Assumenda reiciendis quos ducimus ipsum! Eveniet quisquam distinctio aut sint fugit mollitia explicabo, sequi repellendus, aliquid laudantium, veritatis non voluptatibus quidem!",
@@ -19,9 +19,9 @@ export const ImagesData = [
   },
   {
     imgPreview:
-      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Course-shop-project.png?alt=media&token=b554cfac-8725-430b-8ed2-db745f34a294",
     imgUrl:
-      "https://cdn.pixabay.com/photo/2021/02/07/15/47/reed-5991717_960_720.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Course-shop-tablet.jpg?alt=media&token=142f11d3-e322-4a2f-a8ac-3ab2fda74e8c",
     title: "Course shop",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo unde dolores aliquam iure assumenda ad totam harum, eveniet voluptatibus ipsam culpa corporis reprehenderit enim laudantium accusamus vitae tempora consectetur, odit obcaecati dolor rerum similique! Placeat perspiciatis id maxime consectetur officiis quisquam, exercitationem, recusandae soluta quam, fugit ab temporibus excepturi omnis?",
@@ -36,9 +36,9 @@ export const ImagesData = [
   },
   {
     imgPreview:
-      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Responsive-design-1.png?alt=media&token=df3dd6d2-5521-4374-9916-b57cc45d1a50",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Weather-app-image.png?alt=media&token=d8a6bb78-f91d-4409-90c9-baef9e3e61b9",
     imgUrl:
-      "https://cdn.pixabay.com/photo/2020/10/14/19/49/santorini-5655299_960_720.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-628b9.appspot.com/o/Weather-app-tablet.jpg?alt=media&token=fa343112-bb0c-439f-9359-0123ea8b54bc",
     title: "Weather app",
     description:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident consequatur nam saepe tenetur. Enim ut nesciunt similique architecto amet sed placeat cupiditate cumque rem quos delectus ipsam voluptate, illo hic distinctio expedita quasi molestiae obcaecati maxime voluptates mollitia! Fuga, numquam. Deleniti earum vel autem excepturi, iste alias accusamus sunt!",
