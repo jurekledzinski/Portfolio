6d77a38229fe4da51ee8c9209f7b5251c67b0de2
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-09-17 13:28:50 +0200

    Add serve static files

--- app.js
+++ app.js
@@ -54,6 +54,13 @@ app.use(function (req, res, next) {
 
 app.use("/email", emailRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
 
